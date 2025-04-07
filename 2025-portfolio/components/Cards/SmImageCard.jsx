3150602785455b86c8a3e6885aaa1272fda001f1
Shania Chacon <chacon1shania@gmail.com>
2025-04-07 17:31:13 -0400

    removed daisy plugin

--- 2025-portfolio/components/Cards/SmImageCard.jsx
+++ 2025-portfolio/components/Cards/SmImageCard.jsx
@@ -2,13 +2,20 @@ import React from "react";
 
 function SmImageCard({ imageUrl, link, alt }) {
   return (
-    <div className="bg-neutral-100 rounded-lg w-full h-full">
-      <img
-        src={imageUrl}
-        alt={alt}
-        className="w-full h-full object-cover rounded-lg"
-      />
-    </div>
+    <a
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      className="block transform transition duration-300 hover:scale-105 w-full h-full"
+    >
+      <div className="bg-neutral-100 rounded-lg w-full h-full">
+        <img
+          src={imageUrl}
+          alt={alt}
+          className="w-full h-full object-cover rounded-lg"
+        />
+      </div>
+    </a>
   );
 }
 
